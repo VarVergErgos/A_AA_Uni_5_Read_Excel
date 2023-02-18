bdc11aa98d4e4c882de68d3a45f15618854384dd
Siegfried Boeken <37549326+VarVergErgos@users.noreply.github.com>
2023-02-18 12:27:00 +0100

    reader csv json html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatTableModule
+ 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
